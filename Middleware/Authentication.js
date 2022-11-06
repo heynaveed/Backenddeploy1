a0636003fac46673d00917ce4df0c93debd8980f
heynaveed <naveedpasha0058@gmail.com>
2022-11-06 17:27:54 +0530

    Edited Token

--- Middleware/Authentication.js
+++ Middleware/Authentication.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const Authentication =(req,res,next)=>{
-const token=req.headers?.authorization?.split(" ")[1];
+const token=req.headers;
     var decoded = jwt.verify(token,process.env.SECRET);
     const user_id=decoded.user_id;
    if(decoded){
