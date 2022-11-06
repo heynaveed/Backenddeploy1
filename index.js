5f4b7a7f678d243ddd7f48c8d3b310bfec947219
heynaveed <naveedpasha0058@gmail.com>
2022-11-06 13:18:47 +0530

    added about end point

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get ("/",(req,res)=>{
     res.send("Hello from backend");
 })
 
+app.get("/about",(req,res)=>{
+    res.send("about page is here");
+})
+
 app.listen(PORT,()=>{
     console.log(`Listening to Port ${PORT}`)
 })
